3e72d6511882429f1aa2a9319e706c1ceb81c0fe
henrique <hgphema@gmail.com>
2020-01-17 15:21:56 -0300

    add heroku link

--- README.md
+++ README.md
@@ -5,4 +5,14 @@ is an important technique used to produce polymer materials like the polyamides 
 Many chemical reaction can be employed to proceed the condensation, such as esterification, amidation,
 urethanes, etc. However the kinematics of those reactions still the same.
 
-This simple app shows the mass distribution of the polymer, an important feature to controll the material properties,  in therms of mass and the stechiometric balance initial.
+This simple app shows the mass distribution of the polymer and the degree of polymerization during the transformations on reactor, an important feature to control the  final material properties. The mathematical model is given by Carothers and Flory studies.
+
+# Using Locally
+
+Download this repository then execute:
+> bokeh serve myapp/ --show
+
+# Running online
+
+You can access the app at :
+> polycondensation.herokuapp.com/
--- myapp/main.py
+++ myapp/main.py
@@ -1,10 +1,9 @@
-#---Bibliotecas---#
-import numpy as np #Álgebra
+#---Libraries---#
+import numpy as np
 from bokeh.io import output_file, show, curdoc
 from bokeh.layouts import gridplot, column, row
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource, Slider, TextInput
-from bokeh.models.widgets import Div
 from bokeh.resources import CDN
 from bokeh.embed import file_html
 
@@ -18,18 +17,22 @@ DP = (r + 1)/(1 + r - 2*p*r)
 M =  Mo/(1.0-p)
 
 source1 = ColumnDataSource(data=dict(x=p,y=DP))
-source2 = ColumnDataSource(data=dict(x=p, y=M))
+source2 = ColumnDataSource(data=dict(x=p,y=M))
 
-#Gráficos
-fig1 = figure(plot_width=400, plot_height=250, title='DP  1+r/1+r-2pr')
+#Figs
+fig1 = figure(plot_width=400, plot_height=250, title='Degree of polymerization in function of p')
 fig1.line('x','y', source=source1, color="navy", alpha=0.5)
+fig1.yaxis.axis_label = "DP"
+fig1.xaxis.axis_label = "p"
 
-fig2 = figure(plot_width=400, plot_height=250, title='M  M_o/{1-p}')
+fig2 = figure(plot_width=400, plot_height=250, title='Mass in function of p')
 fig2.line('x','y', source=source2, color="navy", alpha=0.5)
+fig2.yaxis.axis_label = "M"
+fig2.xaxis.axis_label = "p"
 
 fig = gridplot([[fig1,fig2]])
 
-# Sliders para os parâmetros de entrada 'Mo', 'p' e 'r'
+# Sliders to the parameters 'Mo', 'p' e 'r'
 mass = Slider(start=0, end=400, value=1, step=.1, title="Massa Molar (Mo)")
 extention = Slider(start=0,end=0.999, step=0.01, value=0, title="Extensão da reação de polimerização (p)")
 imbalance = Slider(start=0, end=1, step=0.01, value=0, title="Fator de desbalanço estequiométrico(r)")
@@ -40,6 +43,7 @@ def update_data(attrname, old, new):
     Mo = np.linspace(0,mass.value,N)
     p = np.linspace(0,extention.value,N)
     r = np.linspace(0,imbalance.value,N)
+
     DP = (r + 1)/(1 + r - 2*p*r)
     M =  Mo/(1.0-p)
 
@@ -49,12 +53,7 @@ def update_data(attrname, old, new):
 for w in [mass, extention, imbalance]:
     w.on_change('value', update_data)
 
-# Div model with background theory
-div = Div(text="""<b><h>I'll put some text here</b></h>""", width=550, height=110)
-
-
-
 # Set up layouts and add to document
 layout = column(mass, extention, imbalance)
-curdoc().add_root(row(column(div,layout), fig, width=800))
+curdoc().add_root(row(layout, fig, width=800))
 curdoc().title = "Polimerizator 1.1"
